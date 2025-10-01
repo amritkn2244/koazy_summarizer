5384bca824773cb25e9ada3e0011f6c2f6d51e7a
Amrit Kumar Nayak <amritkn2244@gmail.com>
2025-10-01 21:49:36 +0530

    Update backend.py

--- Backend/backend.py
+++ Backend/backend.py
@@ -18,8 +18,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # or ["http://localhost:3000"] for more security
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
